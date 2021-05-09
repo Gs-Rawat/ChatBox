d731d34ec6da4605aeadd23e279016527a81b7e5
Gs Rawat <gsrawat0512@gmail.com>
2021-05-09 20:46:43 +0530

    Add the update command which keep tracks of new messages and fix some bugs.

--- src/com/gsrawat/chatbox/ClientHandler.java
+++ src/com/gsrawat/chatbox/ClientHandler.java
@@ -16,23 +16,28 @@ public class ClientHandler implements Observer, Runnable {
     private String clientCountry;
     private int clientCode;
     private Date date;
+    private int updateMsgCount;
 
     public ClientHandler(Socket client, int clientCode) {
         this.client = client;
         this.clientCode = clientCode;
         this.msgList = new ArrayList<>();
         this.reader = new BufferedReader(new InputStreamReader(System.in));
+        this.date = new Date();
+        this.updateMsgCount = 0;
+        setReaderWriter();
+    }
+
+    public void registerMe() {
         this.broadcastServer = BroadcastServer.getBroadCastServer();
         this.broadcastServer.registerObserver(this);
-        this.date = new Date();
-        setReaderWriter();
     }
 
     public void setReaderWriter() {
         try {
             this.dataReader = new DataInputStream(client.getInputStream());
             this.dataWriter = new DataOutputStream(client.getOutputStream());
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -41,11 +46,12 @@ public class ClientHandler implements Observer, Runnable {
     public void update(Subject s) {
         BroadcastServer broadcastServer = (BroadcastServer) s;
         msgList.add(broadcastServer.getMessage());
+        ++updateMsgCount;
     }
 
     public void readProcessWrite() {
         try {
-            while(true) {
+            while (true) {
                 //read
                 String msg = dataReader.readUTF();
                 //process
@@ -53,12 +59,12 @@ public class ClientHandler implements Observer, Runnable {
                 //write
                 dataWriter.writeUTF(out);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void manageLogin() {
+    public String manageLogin() {
         try {
             dataWriter.writeUTF("Enter your name: ");
             this.clientName = dataReader.readUTF();
@@ -66,30 +72,52 @@ public class ClientHandler implements Observer, Runnable {
             dataWriter.writeUTF("Enter you country: ");
             this.clientCountry = dataReader.readUTF();
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        registerMe();
+        return "Login successful " + "[" + clientName + " from " + clientCountry + "]";
+    }
+
+    public String sendMsg(String msg) {
+        msg = msg.substring("send_msg:".length()).trim();
+        String time = date.toString();
+        String start = "[" + clientName + ": " + time + "] ";
+        broadcastServer.sendMessage(start + msg);
+        --updateMsgCount;
+        return "Message has been sent.";
+    }
+
+    public String getMsg() {
+        StringBuilder builder = new StringBuilder();
+        for (String str : msgList) {
+            builder.append(str).append("\n");
+        }
+        updateMsgCount = 0;
+        return builder.toString();
+    }
+
+    public String updateHandler() {
+        if(updateMsgCount == 0) {
+            return "No new messages. You are up to date.";
+        }
+        return "UPDATE: " + updateMsgCount + " new message.";
     }
 
     public String process(String msg) {
-        StringBuilder builder = new StringBuilder();
 
-        if(msg.startsWith("send_msg:")) {
-            msg = msg.substring("send_msg:".length()).trim();
-            String time = date.toString();
-            String start = "[" + clientName + ": " + time + "] ";
-            broadcastServer.sendMessage(start + msg);
-            builder.append("Message Send.");
+        if (msg.startsWith("send_msg:")) {
+            return sendMsg(msg);
+        } else if (msg.startsWith("get_msg:")) {
+            return getMsg();
+        } else if (msg.startsWith("login:")) {
+            return manageLogin();
+        } else if(msg.startsWith("update:")) {
+            return updateHandler();
+        } else {
+            return "Invalid command";
         }
-        else if(msg.startsWith("get_msg:")) {
-            for(String str : msgList) {
-                builder.append(str).append("\n");
-            }
-        } else if(msg.startsWith("login:")) {
-            manageLogin();
-            builder.append("Login successful " + "[").append(clientName).append(" from ").append(clientCountry).append("]");
-        }
-        return builder.toString();
     }
 
     @Override
